0a30a930de8f19a7b9792b33f821151180a65ce7
Keinssz <116791211+Keinssz@users.noreply.github.com>
2023-01-12 09:46:10 +0100

    Add files via upload

---
+++ Series.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+public static class Series
+{
+    public static string[] Slices(string numbers, int sliceLength)
+    {// in caso di errore eseguire questa riga di codice per comuncare l'errore al chiamante!!   
+        List<string> retVal = new List<string>();
+        if(sliceLength>numbers.Length){throw new ArgumentException();}
+        if(sliceLength==0){throw new ArgumentException();}
+        if(sliceLength<0){throw new ArgumentException();}
+
+        if(numbers.Length>=sliceLength)
+        {
+            for(int i=0;i<numbers.Length-(sliceLength-1);i++)
+            {
+                string s = numbers.Substring(i, sliceLength);
+                retVal.Add(s);
+            }
+        }
+        
+        return retVal.ToArray();
+    }
+}
